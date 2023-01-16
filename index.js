d24bef01818dd8ded608b9318a03d1f4e7c0fd01
Shivam-Pant <pantshivam07@gmail.com>
2023-01-16 19:59:45 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,6 @@
 ReactDOM.render(<h1>I Have Done It</h1>, document.getElementById("Block"))
 ReactDOM.render(<p>Hi I'm Learning New Things</p>, document.querySelector("#Call"))
+ReactDOM.render(
+                <ul><li>Work Done</li><li>Work Not Done</li></ul>,
+                document.getElementById("List")
+                )
