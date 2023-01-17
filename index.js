47ef44d946126cd71e9f9a8808f2ac84d03b6501
Shivam-Pant <pantshivam07@gmail.com>
2023-01-17 11:22:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,3 +17,30 @@ const h1 = document.createElement("h1")
 h1.textContent = "Do Your Work Easily And Fast"
 h1.className = "header"
 document.getElementById("Block").append(h1)
+const h2 = document.createElement("h2")
+h2.textcontent = "Do Your Work Or Get Out"
+h2.className = "lower"
+console.log(h2)
+const element = <h2 className="lower">This Is Not Good</h2>
+console.log(element)
+ReactDOM.render(element, document.getElementById("Rock"))
+const tier = (
+           <div>
+            <h1>Working</h1>
+            <p>This Is Done By Myself</p>
+           </div>
+
+)
+ReactDOM.render(tier, document.querySelector("#Work"))
+const navbar = (
+    <nav>
+        <h1> StarLucks </h1>
+        <ul>
+            <li>Menu</li>
+            <li>About</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+)
+
+ReactDOM.render(navbar, document.getElementById("Root"))
